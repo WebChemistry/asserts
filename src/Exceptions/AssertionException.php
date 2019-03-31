558fe05e089197f83ffee8a614bdc4bfa0f52737
MartkCz <antik494@gmail.com>
2019-03-31 13:20:31 +0200

    initial commit

---
+++ src/Exceptions/AssertionException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types = 1);
+
+namespace WebChemistry\Asserts\Exceptions;
+
+use RuntimeException;
+
+class AssertionException extends RuntimeException {
+
+}
---
+++ src/FileSystemAssert.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types = 1);
+
+namespace WebChemistry\Asserts;
+
+use Nette\StaticClass;
+use WebChemistry\Asserts\Exceptions\AssertionException;
+
+class FileSystemAssert {
+
+	use StaticClass;
+
+	public static function fileExists(string $filePath): void {
+		if (!@is_file($filePath)) {
+			throw new AssertionException("File '$filePath' not exists.");
+		}
+	}
+
+}
